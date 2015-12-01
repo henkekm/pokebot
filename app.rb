66f3877f4b279c411026f2502645d1f6e3366866
Kevin Henke <henkekm@gmail.com>
2015-12-01 13:22:06 -0500

    Simplifies for testing

--- app.rb
+++ app.rb
@@ -9,19 +9,21 @@ require 'json'
 
 post '/gateway' do
   message = params[:text].gsub(params[:trigger_word], '').strip
-  puts message
+  # puts message
 
   action, poke = message.split('_').map {|c| c.strip.downcase }
 
   uri_directory, pokenumber = 'pokemon', '1'
-  api_url = "http://pokeapi.co/api/v1/#{uri_directory}/#{pokenumber}"
+  # api_url = "http://pokeapi.co/api/v1/#{uri_directory}/#{pokenumber}"
+  api_url = "http://pokeapi.co/api/v1/pokemon/1"
 
   case action
 		# return if params[:token] != ENV['7FP2gNGr1zMck7T9eUAfGTlI']
     when 'pokemon'
       response = HTTParty.get(api_url)
       response = JSON.parse response.body
-      respond_message "Meet #{response[name]}. #{response[name]} is a #{response[species]}, with a speed of #{response[speed]} to start."
+      # respond_message "Meet #{response[name]}. #{response[name]} is a #{response[species]}, with a speed of #{response[speed]} to start."
+      respond_message "Stock Response"
   end
 end
 
