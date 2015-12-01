7647d965e583070806c563982606ed2252188251
Kevin Henke <henkekm@gmail.com>
2015-12-01 13:13:23 -0500

    Renames variables, includes tut reference at top

--- app.rb
+++ app.rb
@@ -1,21 +1,27 @@
 require 'sinatra'
 require 'httparty'
 require 'json'
+# Message:-
+#     giubot: issues _ twbs/bootstrap
+
+# Response:-
+#     There are currently 433 open issues on twbs/bootstrap.
 
 post '/gateway' do
   message = params[:text].gsub(params[:trigger_word], '').strip
   puts message
 
-  action = message.split('_').map {|c| c.strip.downcase }
+  action, poke = message.split('_').map {|c| c.strip.downcase }
 
-  repo, pokenumber = 'pokemon', '1'
-  repo_url = "http://pokeapi.co/{repo}/{pokenumber}"
+  uri_directory, pokenumber = 'pokemon', '1'
+  api_url = "http://pokeapi.co/api/v1/#{uri_directory}/#{pokenumber}"
 
   case action
-    when 'bulbasaur'
-      response = HTTParty.get(repo_url)
+		# return if params[:token] != ENV['7FP2gNGr1zMck7T9eUAfGTlI']
+    when 'pokemon'
+      response = HTTParty.get(api_url)
       response = JSON.parse response.body
-      respond_message "Meet {response[name]}. {response[name]} is a {response[species]}, with a speed of {response[speed]} to start."
+      respond_message "Meet #{response[name]}. #{response[name]} is a #{response[species]}, with a speed of #{response[speed]} to start."
   end
 end
 
