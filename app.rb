3c8b44756e0d35edfb9d7d2162fd63d61166b684
Kevin Henke <henkekm@gmail.com>
2015-12-01 13:45:28 -0500

    Adds number, move, and ability params to action

--- app.rb
+++ app.rb
@@ -11,19 +11,41 @@ post '/gateway' do
   message = params[:text].gsub(params[:trigger_word], '').strip
   # puts message
 
-  action, poke = message.split('_').map {|c| c.strip.downcase }
+  action, arg = message.split('_').map {|c| c.strip.downcase }
 
-  uri_directory, pokenumber = 'pokemon', '1'
+  uri_directory, pokenumber, movenumber, abilitynumber = 'pokemon', '1', '1', '1'
   # api_url = "http://pokeapi.co/api/v1/#{uri_directory}/#{pokenumber}"
-  api_url = "http://pokeapi.co/api/v1/pokemon/1"
 
   case action
 		# return if params[:token] != ENV['7FP2gNGr1zMck7T9eUAfGTlI']
     when 'pokemon'
+    	# pokenumber = {pokemon lookup to convert arg from string to a number}
+    	uri_directory = 'pokemon'
+		  api_url = "http://pokeapi.co/api/v1/#{uri_directory}/#{pokenumber}"
       response = HTTParty.get(api_url)
       response = JSON.parse response.body
       respond_message "Meet #{response['name']}. #{response['name']} is a #{response['species']}, with a speed of #{response['speed']} to start."
-      # respond_message "Stock Response #{response}"
+    when 'number'
+    	pokenumber = arg
+    	uri_directory = 'pokemon'
+		  api_url = "http://pokeapi.co/api/v1/#{uri_directory}/#{pokenumber}"
+      response = HTTParty.get(api_url)
+      response = JSON.parse response.body
+      respond_message "Meet #{response['name']}. #{response['name']} is a #{response['species']}, with a speed of #{response['speed']} to start."
+    when 'move'
+    	movenumber = arg
+    	uri_directory = 'move'
+		  api_url = "http://pokeapi.co/api/v1/#{uri_directory}/#{movenumber}"
+      response = HTTParty.get(api_url)
+      response = JSON.parse response.body
+      respond_message "Move #{response['id']} is called #{response['name']}, has an accuracy of #{response['accuracy']}, and has a power of #{response['power']}"
+    when 'ability'
+    	abilitynumber = arg
+    	uri_directory = 'ability'
+		  api_url = "http://pokeapi.co/api/v1/#{uri_directory}/#{abilitynumber}"
+      response = HTTParty.get(api_url)
+      response = JSON.parse response.body
+      respond_message "Ability #{response['id']} is #{response['name']}. #{response['name']} is a so cool."
   end
 end
 
